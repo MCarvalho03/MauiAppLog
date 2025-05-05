ac57155d37f4878e3646af36660d3d9dc4cfc8f4
Nyktos <Nyktos@DESKTOP-R8CLOVV>
2025-05-05 12:10:26 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppLog/Behind.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiAppLog;
+
+public partial class NewPage1 : ContentPage
+{
+	public NewPage1()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MauiAppLog/Login.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiAppLog;
+
+public partial class Login : ContentPage
+{
+	public Login()
+	{
+		InitializeComponent();
+	}
+}
